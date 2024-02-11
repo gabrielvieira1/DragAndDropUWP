35bc2519c58ad1ef212499bf966cace84dbb98ef
Gabriel Vieira <gabrielvieira1@protonmail.com>
2024-02-10 22:22:17 -0300

    How to use drag and drop

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    private void Border_DragOver(object sender, DragEventArgs e)
+    {
+      if (e.DataView.Contains(StandardDataFormats.StorageItems) || e.DataView.Contains(StandardDataFormats.Text))
+        e.AcceptedOperation = DataPackageOperation.Copy;
+
+      e.DragUIOverride.Caption = "Custom caption";
+      e.DragUIOverride.IsCaptionVisible = true;
+      e.DragUIOverride.IsContentVisible = false;
+      e.DragUIOverride.IsGlyphVisible = true;
+    }
+
+    private async void Border_Drop(object sender, DragEventArgs e)
+    {
+      if (e.DataView.Contains(StandardDataFormats.StorageItems))
+      {
+        var items = await e.DataView.GetStorageItemsAsync();
+        DraggedText.Text = "";
+        foreach (var item in items)
+        {
+          var storageFile = item as StorageFile;
+          var text = await FileIO.ReadTextAsync(storageFile);
+          DraggedText.Text += text;
+        }
+      }
+      if (e.DataView.Contains(StandardDataFormats.Text))
+      {
+        var text = await e.DataView.GetTextAsync();
+        DraggedText.Text = text;
+      }
+    }
+
+    private void Border_DragStarting(UIElement sender, DragStartingEventArgs args)
+    {
+      args.AllowedOperations = DataPackageOperation.Copy;
+      args.Data.SetText(SourceText.Text);
+    }
+  }
+}
